9c2040789b5bb0b517307c4087bd9e512f5dcef1
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-08-24 13:18:26 +0200

    set up pixel camera

---
+++ src/consts.rs
@@ -0,0 +1,3 @@
+// for screen res i wanted something 16:9 thats also divisble by 16
+pub const SCREEN_WIDTH: f32 = 256.0;
+pub const SCREEN_HEIGHT: f32 = 144.0;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use macroquad::prelude::*;
+use macroquad::{miniquad::window::screen_size, prelude::*};
 
 mod assets;
+mod consts;
 use assets::*;
+use consts::*;
 
 struct Ramble<'a> {
     assets: &'a Assets,
@@ -10,8 +12,33 @@ struct Ramble<'a> {
 #[macroquad::main("ramble")]
 async fn main() {
     let assets = Assets::default();
+    let render_target = render_target(SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32);
+    render_target.texture.set_filter(FilterMode::Nearest);
+    let pixel_camera = Camera2D {
+        render_target: Some(render_target),
+        zoom: Vec2::new(1.0 / SCREEN_WIDTH * 2.0, 1.0 / SCREEN_HEIGHT * 2.0),
+        target: Vec2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
+        ..Default::default()
+    };
     loop {
+        let (screen_width, screen_height) = screen_size();
+        let scale_factor = (screen_width / SCREEN_WIDTH).min(screen_height / SCREEN_HEIGHT);
+        set_camera(&pixel_camera);
         draw_texture(&assets.entities, 0.0, 0.0, WHITE);
+        set_default_camera();
+        draw_texture_ex(
+            &pixel_camera.render_target.as_ref().unwrap().texture,
+            0.0,
+            0.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(
+                    SCREEN_WIDTH * scale_factor,
+                    SCREEN_HEIGHT * scale_factor,
+                )),
+                ..Default::default()
+            },
+        );
         next_frame().await
     }
 }
